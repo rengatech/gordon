ee1a038f6ca1cffb4040a9a6c6ae42cb0fb93726
Asraf313 <mohamedasraf323@gmail.com>
2024-03-21 06:44:11 -0700

    updated

--- resources/js/Components/CustomGordon/Footer.vue
+++ resources/js/Components/CustomGordon/Footer.vue
@@ -2,8 +2,8 @@
 </script>
 
 <template>
-<div class="mx-auto m-[60%] mt-[20%] bg-zinc-800 ">
-        <div class="ml-60 p-8 top-20% text-gray-600">
+<div class="mt-20  bg-zinc-800 ">
+        <div class="ml-60 p-8  text-gray-600">
         <p>Payment Policy Terms and conditions</p>
         <p>© 2018, All Rights Reserved.</p>
       </div>
