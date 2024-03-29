ef9ca9ca5faaed49e771bd9df3e2cfa44a131fa6
Asraf313 <mohamedasraf323@gmail.com>
2024-03-28 19:01:15 -0700

    Confirmation Details

--- resources/js/Components/CustomGordon/Footer.vue
+++ resources/js/Components/CustomGordon/Footer.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-<div class="mt-[15%]  bg-zinc-800 ">
+<div class="mt-[50%]  bg-zinc-800 ">
         <div class="md:pl-60 justify-center items-center  p-8  text-gray-600">
         <p>Payment Policy Terms and conditions</p>
         <p>© 2018, All Rights Reserved.</p>
--- resources/js/Components/CustomGordon/HomePage.vue
+++ resources/js/Components/CustomGordon/HomePage.vue
@@ -5,18 +5,19 @@
     <div >
         <div>
       <div class="md:absolute left-2 md:top-[50%] md:left-40">
-        <div class="absolute  left-2 md:left-20 mt-[26rem] md:mt-20 border-b-2 pb-10 bg-white shadow-2xl w-[23rem] md:w-96 bg-opacity-90 rounded-3xl">
+        <div class="absolute  left-2 md:left-20 mt-[26rem] md:mt-20 border-b-2 pb-10 bg-white shadow-2xl w-[23rem] md:w-96 bg-opacity-90 rounded-[40px]">
           <div>
-            <div class="left-2 md:top-20 md:left-20 flex justify-between mt-8 p-3 border-b-2  border-[#014F99]">
+            <div class="left-2 md:top-20 md:left-20 flex justify-between md:mt-8 mt-5 p-3 border-b-2 ">
               <div>
-                <img src="images/Mask.png" class="w-50" />
+                <img src="images/Mask.png" class="w-40 " />
               </div>
               <div>
                 <img src="images/logo.png" class="w-26" />
               </div>
             </div>
+            <div class="border-2 border-b-[#014F99] mt-6 md:mt-0"></div>
           </div>
-          <div class="p-4">
+          <div class="p-4 ml-2 ">
           <h1 class="text-2xl font-bold mt-5">Pastor Mo Huggins</h1>
           <h2 class="mt-8">Price includes:</h2>
           <p class="max-w-xs text-left text-slate-500 text-bold">
@@ -37,6 +38,9 @@
             <h1 class="font-bold text-xl">$4,500</h1>
             <p>Price per person</p>
           </button>
+          <div>
+                <img src="images/line 1.png" class="w-26 mt-8" />
+            </div>
           <h1 class="mt-5 text-gray-600">Single Room Supplement- $940</h1>
             <p class="text-xs text-gray-400">Single supplement - If You want to take a room alone.</p>
             <h2 class="mt-5 text-gray-600">Land Only - $2492</h2>
